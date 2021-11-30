14641e114cd675944d6c8a15586ac407656676b5
John Sullivan <jsullivan@csumb.edu>
2021-11-29 19:30:50 -0800

    You thought that was final?

--- js/titler.js
+++ js/titler.js
@@ -3,12 +3,7 @@ function titler() {
 		"The world's greatest image stegonography tool.",
 		"Your secrets are not safe with us.",
 		"Shame.",
-		"Your number one source for evading the NSA.",
-		"Big brother is watching.",
-		"Images of your Gov. Weiner don't work.",
-		"Use any image of the President for a surprise.",
 		"Why are you here?",
-		"We don't negotiate with terrorists.",
 		"Your packets have been diverted, consequences will never be the same.",
 		"Nobody cared what the file was until you hid it."
 	];
